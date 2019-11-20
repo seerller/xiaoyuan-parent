39444cdbe9db40941017d845c331abb0806fe203
tangzhenye <1139880520@qq.com>
2019-11-20 17:20:59 +0800

    model changes

---
+++ xiaoyuan-model/src/main/java/com/xiaoyuan/model/UserInfo.java
@@ -0,0 +1,13 @@
+package com.xiaoyuan.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "tl_user_info")
+public class UserInfo {
+
+}
